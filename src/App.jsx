44d8a9a735c248a4e10a39df9329cf3304cc3c5a
sdhital12 <dhitalsita1997@gmail.com>
2023-08-18 22:53:13 -0400

    my commit

---
+++ src/App.jsx
@@ -0,0 +1,53 @@
+import './App.css'
+
+function App() {
+
+
+  return (
+    <><header>
+      <div className='menu'>
+
+        <div><img width={'90px'} src='https://th.bing.com/th/id/OIP.qFIEgvi4zQILveyB28veXgHaHa?pid=ImgDet&w=178&h=178&c=7&dpr=1.3'/></div>
+        <div className='items'>
+        <ul>
+          <li>Menu</li>
+           <li>Categories</li>
+            <li>Offers</li>
+             <li>Daily Deals</li>
+              <li>Contact</li>
+
+        </ul>
+        </div>
+
+      </div>
+      </header>
+
+      <main className='flex'>
+        <div className='description'>
+          <h1>Welcome to my Coffee Shop</h1>
+          <p>Coffee is a beverage prepared from roasted coffee beans. Darkly colored, bitter, and slightly acidic, coffee has a stimulating effect on humans, primarily due to its caffeine content. It has the highest sales in the world market for hot drinks.[2]
+
+The seeds of the Coffea plant's fruits are separated to produce unroasted green coffee beans. The beans are roasted and then ground into fine particles that are typically steeped in hot water before being filtered out, producing a cup of coffee. It is usually served hot, although chilled or iced coffee is common. Coffee can be prepared and presented in a variety of ways (e.g., espresso, French press, caffè latte, or already-brewed canned coffee). Sugar, sugar substitutes, milk, and cream are 
+ added to mask the bitter taste or enhance the flavor. <br/>
+ The seeds of the Coffea plant's fruits are separated to produce unroasted green coffee beans. The beans are roasted and then ground into fine particles that are typically steeped in hot water before being filtered out, producing a cup of coffee. It is usually served hot, although chilled or iced coffee is common. Coffee can be prepared and presented in a variety of ways (e.g., espresso, French press, caffè latte, or already-brewed canned coffee). Sugar, sugar substitutes, milk, and cream are 
+ added to mask the bitter taste or enhance the flavor
+ </p>
+        </div>
+        <div><img src='https://th.bing.com/th?id=ALSTU1C05162DF22F2C2E8EF4206C8DBBEA9FA785433DDD73D0BBEB59978C47020D96&w=472&h=280&rs=2&o=6&oif=webp&dpr=1.3&pid=SANGAM'/></div>
+      </main>
+
+      <article>
+      Coffee is a beverage prepared from roasted coffee beans. Darkly colored, bitter, and slightly acidic, coffee has a stimulating effect on humans, primarily due to its caffeine content. It has the highest sales in the world market for hot drinks.[2]
+
+The seeds of the Coffea plant's fruits are separated to produce unroasted green coffee beans. The beans are roasted and then ground into fine particles that are typically steeped in hot water before being filtered out, producing a cup of coffee. It is usually served hot, although chilled or iced coffee is common. Coffee can be prepared and presented in a variety of ways (e.g., espresso, French press, caffè latte, or already-brewed canned coffee). Sugar, sugar substitutes, milk, and cream are 
+ added to mask the bitter taste or enhance the flavor. <br/>
+ The seeds of the Coffea plant's fruits are separated to produce unroasted green coffee beans. The beans are roasted and then ground into fine particles that are typically steeped in hot water before being filtered out, producing a cup of coffee. It is usually served hot, although chilled or iced coffee is common. Coffee can be prepared and presented in a variety of ways (e.g., espresso, French press, caffè latte, or already-brewed canned coffee). Sugar, sugar substitutes, milk, and cream are 
+ added to mask the bitter taste or enhance the flavor
+      </article>
+
+      <footer>© copyright coffee shop</footer>
+    </>
+  )
+}
+
+export default App
